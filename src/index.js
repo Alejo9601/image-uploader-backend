a309bb29a05f90083af50201fe85e1a8a5717067
Alejo9601 <alejandro96jm@gmail.com>
2022-08-02 17:12:55 -0300

    First commit to backend

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
+
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+const PORT = 3001;
+
+app.post("/upload", upload.single("myFile"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send(file);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
