9007b6f89792daf29b75cff804acae246e456f1f
Alejo9601 <alejandro96jm@gmail.com>
2022-08-04 09:11:47 -0300

    Added route to delete all files uploaded on path /delete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
 const fs = require("fs");
-const dir = "./uploads";
+const fsP = require("fs").promises;
+const path = require("path");
 
+const dir = "./uploads";
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
@@ -30,6 +32,25 @@ const PORT = process.env.PORT || 3001;
 app.post("/upload", upload.single("myFile"), (req, res) => {
   const file = req.file;
   res.send(file);
+});
+
+app.delete("/delete", (req, res) => {
+  const FOLDER_TO_REMOVE = "uploads";
+
+  fsP
+    .readdir(FOLDER_TO_REMOVE)
+    .then((files) => {
+      const unlinkPromises = files.map((file) => {
+        const filePath = path.join(FOLDER_TO_REMOVE, file);
+        return fsP.unlink(filePath);
+      });
+      return Promise.all(unlinkPromises).then(res.status(204).end());
+    })
+    .catch((err) => {
+      console.error(
+        `Something wrong happened removing files of ${FOLDER_TO_REMOVE}`
+      );
+    });
 });
 
 app.listen(PORT, () => {
