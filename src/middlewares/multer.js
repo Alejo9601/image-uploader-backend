b5765befb4e353954d7aa87db1cecedb2d4544b1
Alejo9601 <alejandro96jm@gmail.com>
2022-08-08 17:48:13 -0300

    Changed the way file names are defined

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -5,8 +5,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    console.log(file);
-    cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, file.originalname);
   },
 });
 
